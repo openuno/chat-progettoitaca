8ad620cabb99fd71c31beab86fc53c9a88087315
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-05-27 08:51:07 +0200

    Initial WORKING CHAT

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 #!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
 var WebSocketServer = require('websocket').server;
 var http=require('http');
-/**
- *  Define the sample application.
- */
-
 function originIsAllowed(origin) {
   // put logic here to detect whether the specified origin is allowed.
   return true;
@@ -15,23 +10,11 @@ function originIsAllowed(origin) {
 var cclients=[];
 var cclientnum=0
 var SampleApp = function() {
-
-    //  Scope.
     var self = this;
-
-
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
     self.setupVariables = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
         self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
@@ -39,28 +22,14 @@ var SampleApp = function() {
             self.ipaddress = "127.0.0.1";
         };
     };
-
-
-    /**
-     *  Populate the cache.
-     */
     self.populateCache = function() {
         if (typeof self.zcache === "undefined") {
             self.zcache = { 'index.html': '' };
         }
-
         //  Local cache for static content.
         self.zcache['index.html'] = fs.readFileSync('./index.html');
     };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
     self.cache_get = function(key) { return self.zcache[key]; };
-
-
     /**
      *  terminator === the termination handler
      *  Terminate server on receipt of the specified signal.
@@ -74,15 +43,9 @@ var SampleApp = function() {
         }
         console.log('%s: Node server stopped.', Date(Date.now()) );
     };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
     self.setupTerminationHandlers = function(){
         //  Process on exit and signals.
         process.on('exit', function() { self.terminator(); });
-
         // Removed 'SIGPIPE' from the list - bugz 852598.
         ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
          'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
@@ -90,104 +53,37 @@ var SampleApp = function() {
             process.on(element, function() { self.terminator(element); });
         });
     };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
     self.createRoutes = function() {
         self.routes = { };
-
         self.routes['/asciimo'] = function(req, res) {
             var link = "http://i.imgur.com/kmbjB.png";
             res.send("<html><body><img src='" + link + "'></body></html>");
         };
-
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
         };
     };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
+    self.initializeServer=function(){self.createRoutes();self.app=express.createServer();for(var r in self.routes){self.app.get(r,self.routes[r]);}};
+    self.initialize=function(){self.setupVariables();self.populateCache();self.setupTerminationHandlers();self.initializeServer();};
     self.start = function() {
-        //  Start the app on the specific interface (and port).
        var HSERVER=self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+            console.log('%s: Node server started on %s:%d ...', Date(Date.now() ), self.ipaddress, self.port);
         });
-       wsServer = new WebSocketServer({
-    httpServer: HSERVER,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
-
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-    var cidx=cclients.length;
-    cclients[cidx] = request.accept(null, request.origin);
-    cclientnum++;
-    cclients[cidx].on('message', function(message) {var cc;
-        if (message.type === 'utf8') {
-            if(message.utf8Data.indexOf('#!howmany')==0){cclients[cidx].sendUTF(cclientnum.toString())}else{
-            for(cc=0;cc<cclients.length;cc++){cclients[cc].sendUTF(message.utf8Data);}}
-        }
-        else if (message.type === 'binary') {
-            for(cc=0;cc<cclients.length;cc++){cclients[cc].sendBytes(message.binaryData);}
-        }
-    });
-    cclients[cidx].on('close', function(reasonCode, description) {var cc;
-        for(cc=0;cc<cclients.length;cc++){cclients[cc].sendUTF('One user left.');cclientnum--;}
-    });
-});
+        wsServer=new WebSocketServer({httpServer:HSERVER,autoAcceptConnections:false});
+        wsServer.on('request', function(request) {
+            if(!originIsAllowed(request.origin)){request.reject();return;}
+            var cidx=cclients.length;cclients[cidx]=request.accept(null, request.origin);cclientnum++;
+            cclients[cidx].on('message',function(message){var cc;
+                if(message.type==='utf8'){
+                    if(message.utf8Data.indexOf('#!howmany')==0){cclients[cidx].sendUTF(cclientnum.toString())}
+                    else{for(cc=0;cc<cclients.length;cc++){cclients[cc].sendUTF(message.utf8Data);}}
+                }else if(message.type==='binary'){for(cc=0;cc<cclients.length;cc++){cclients[cc].sendBytes(message.binaryData);}}
+            });
+            cclients[cidx].on('close',function(rcode,desc){var cc;for(cc=0;cc<cclients.length;cc++){cclients[cc].sendUTF('one user left.');cclientnum--;}});
+        });
     };
-
-};   /*  Sample Application.  */
-
+};
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();

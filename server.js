f2541ebca14ee279f90a251ef11d21c260f74f72
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-05-28 00:32:55 +0200

    hjhj

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var SampleApp = function() {
             res.send("<html><body><img src='" + link + "'></body></html>");
         };
         self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
+            res.setHeader('Content-Type', 'text/html'); 
             res.send(self.cache_get('index.html') );
         };
     };
